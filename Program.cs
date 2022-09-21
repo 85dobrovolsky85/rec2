d782bd9432384f9ee7879290e80666d01493b08c
Evgeniy <dobrovolsky.evgey@yandex.ru>
2022-09-21 13:10:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+ 
+string[] array =
+{
+ "hello", "2", "world", ":-)",
+ "1234", "1567", "2", "computer science",
+ "Russia", "Denmark", "Kazan"
+};
+ 
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+ if (value.Length <= 3)
+ {
+ result[realSize] = value;
+ realSize++;
+ }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
